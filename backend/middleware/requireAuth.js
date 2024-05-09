3c1212d82ba9999a3972fb18925ed4d5d5874eff
Sangeetha2125 <gsangeetha7113@gmail.com>
2024-05-09 11:07:56 +0530

    auth header changed

--- backend/middleware/requireAuth.js
+++ backend/middleware/requireAuth.js
@@ -1,7 +1,7 @@
 const jwt = require('jsonwebtoken');
 const User = require("../models/userModel.js");
 const requireAuth = async (req,res,next)=>{
-  const authorization = req.header("Authorization")
+  const {authorization} = req.headers
 
   if(!authorization)
     return res.status(401).json({error:'Authorization token required'})
