58e3a98f8baf1f9957551185ed291575eacf0e3f
Sangeetha2125 <gsangeetha7113@gmail.com>
2024-05-10 20:44:13 +0530

    node geo fix

--- backend/utils/calculateDistance.js
+++ backend/utils/calculateDistance.js
@@ -1,19 +1,21 @@
 const geo = require("node-geo-distance");
-const getDistance = (donorLatitude,donorLongitude,receiverLatitude,receiverLongitude)=>{
- 
-  const coord1 = {
-    latitude: donorLatitude,
-    longitude: donorLongitude
-  }
-   
-  let coord2 = {
-    latitude: receiverLatitude,
-    longitude: receiverLongitude
-  }
-   
-  geo.haversine(coord1, coord2, function(distance) {
-    return distance;
+
+const getDistance = (donorLatitude, donorLongitude, receiverLatitude, receiverLongitude) => {
+  return new Promise((resolve, reject) => {
+    const coord1 = {
+      latitude: donorLatitude,
+      longitude: donorLongitude
+    };
+
+    const coord2 = {
+      latitude: receiverLatitude,
+      longitude: receiverLongitude
+    };
+
+    geo.haversine(coord1, coord2, function(distance) {
+      resolve(distance);
+    });
   });
-}
+};
 
 module.exports = getDistance
