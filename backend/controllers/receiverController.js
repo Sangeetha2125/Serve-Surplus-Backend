d38acb331e9ec14304e863d4d4c757c7bedf97cb
Sangeetha2125 <gsangeetha7113@gmail.com>
2024-05-11 18:24:42 +0530

    final fix: receiver get nearest donations

--- backend/controllers/receiverController.js
+++ backend/controllers/receiverController.js
@@ -4,6 +4,55 @@ const Receivers = require("../models/receiverModel");
 const Users = require("../models/userModel");
 const  geo = require('node-geo-distance');
 
+const getAllNearestDonations = async (req, res) => {
+  try {
+    const receiverId = req.user;
+    const receiver = await Users.findOne({ _id: receiverId });
+    const donors = await Users.find({ role: "Donor" });
+    let nearestDonations = [];
+    for (let i = 0; i < donors.length; i++) {
+      const coord1 = {
+        latitude: donors[i].latitude,
+        longitude: donors[i].longitude,
+      };
+
+      var coord2 = {
+        latitude: receiver.latitude,
+        longitude: receiver.longitude,
+      };
+
+      const distance = geo.haversine(coord1, coord2, function (dist) {
+        return dist;
+      });
+
+      if (distance <= 30000) {
+        const donations = await Donors.findOne({userId:donors[i]._id})
+        let donationInfo = {donor: donors[i]._id,distance: Math.round(distance/1000),donations: donations.donations}
+        nearestDonations = nearestDonations.concat(donationInfo)
+      }
+    }
+    res.status(200).json(nearestDonations);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+const getDonorDetails = async(req,res) => {
+  const {donorId} = req.body
+  try {
+    if(!donorId || donorId===''){
+      return res.status(400).json("Donor id is required to access this route")
+    }
+    const donor = await Users.findOne({_id:donorId}).select('-password -latitude -longitude');
+    if(!donor){
+      return res.status(404).json(`No donor exists with id:${donorId}`)
+    }
+    res.status(200).json(donor)
+  } catch (error) {
+    res.status(500).json(error.message);
+  }
+}
+
 const order = async (req, res) => {
   const { orders } = req.body;
   try {
@@ -61,37 +110,4 @@ const getReceiverOrders = async (req, res) => {
   }
 };
 
-const getAllNearestDonations = async (req, res) => {
-  try {
-    const receiverId = req.user;
-    const receiver = await Users.findOne({ _id: receiverId });
-    const donors = await Users.find({ role: "Donor" });
-    let nearestDonations = [];
-    for (let i = 0; i < donors.length; i++) {
-      const coord1 = {
-        latitude: donors[i].latitude,
-        longitude: donors[i].longitude,
-      };
-
-      var coord2 = {
-        latitude: receiver.latitude,
-        longitude: receiver.longitude,
-      };
-
-      const distance = geo.haversine(coord1, coord2, function (dist) {
-        return dist;
-      });
-
-      if (distance <= 30000) {
-        const donations = await Donors.findOne({userId:donors[i]._id})
-        let donationInfo = {donor: donors[i]._id,distance: Math.round(distance/1000),donations: donations.donations}
-        nearestDonations = nearestDonations.concat(donationInfo)
-      }
-    }
-    res.status(200).json(nearestDonations);
-  } catch (error) {
-    res.status(500).json({ error: error.message });
-  }
-};
-
-module.exports = { order, getReceiverOrders, getAllNearestDonations };
+module.exports = { order, getReceiverOrders, getAllNearestDonations, getDonorDetails };
--- backend/routes/receiverRoutes.js
+++ backend/routes/receiverRoutes.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const {order, getReceiverOrders, getAllNearestDonations} = require("../controllers/receiverController")
+const {order, getReceiverOrders, getAllNearestDonations, getDonorDetails} = require("../controllers/receiverController")
 const requireAuth = require("../middleware/requireAuth")
 const receiverAuth = require("../middleware/receiverAuth")
 
@@ -9,5 +9,6 @@ router.use(receiverAuth)
 router.get("/",getAllNearestDonations);
 router.post("/:id",order);
 router.get("/requests",getReceiverOrders)
+router.get("/donor-info",getDonorDetails)
 
 module.exports = router;
