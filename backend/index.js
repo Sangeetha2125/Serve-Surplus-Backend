baac74bf29b82d065fe024ad28bfd6e751bb1ea7
Sakthilakshmi-M <sakthilakshmims@gmail.com>
2024-05-12 17:46:29 +0530

    mail removed

--- backend/index.js
+++ backend/index.js
@@ -7,15 +7,14 @@ const authRoutes = require("./routes/authRoutes");
 const profileRoutes = require("./routes/profileRoutes")
 const donorRoutes = require("./routes/donorRoutes")
 const receiverRoutes = require("./routes/receiverRoutes")
-const sendEmail = require("./utils/sendEmail");
 app.use(cors());
 app.use(express.json())
 
 //routes
 app.get("/",async(req,res)=>{
-  await sendEmail();
   res.send("Serve Surplus")
 })
+
 app.use("/api/auth",authRoutes);
 app.use("/api/profile",profileRoutes);
 app.use("/api/donor",donorRoutes)
