4c831595a61c87201d36fa6aafc32de5e12752a6
Sangeetha2125 <gsangeetha7113@gmail.com>
2024-05-12 23:11:20 +0530

    confirm order api completed

--- controllers/donorController.js
+++ controllers/donorController.js
@@ -126,7 +126,7 @@ const confirmOrder = async(req,res) => {
     }
     res.status(400).json({message:"Invalid secret provided"})
   } catch (error) {
-    res.status(500).json(error.message);
+    res.status(500).json({message: error.message});
   }
 }
 
